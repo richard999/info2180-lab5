f84117d6f73ba825860202c0ec574c0ec88c90c0
Richard Ebanks <richard.ebanks@mymona.uwi.edu>
2020-11-19 23:44:28 -0500

    City lookup feature

--- world.php
+++ world.php
@@ -1,17 +1,90 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if(isset($_GET['context']) && $_GET['context']=='cities'){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM countries JOIN cities ON countries.code=cities.country_code WHERE countries.name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+  echo "<table id='result-table'>
+          <thead>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>
+                District
+              </th>
+              <th>
+                Population
+              </th>
+            </tr>
+          </thead> 
+          <tbody>
+        ";
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+  foreach ($results as $row){
+    echo "<tr>";
+      echo "<td>";
+        echo $row['name'];
+      echo "</td>";
+      echo "<td>";
+        echo $row['district'];
+      echo "</td>";
+      echo "<td>";
+        echo $row['population'];
+      echo "</td>";
+    echo "</tr>";
+  }
+  echo "</tbody>
+    </table>";
+}
+else{
+  
+  $stmt = $conn->query("SELECT name, continent, independence_year, head_of_state FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  echo "<table id='result-table'>
+          <thead>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>
+                Continent
+              </th>
+              <th>
+                Independence
+              </th>
+              <th>
+                Head of State
+              </th>
+            </tr>
+          </thead> 
+          <tbody>
+        ";
+
+  foreach ($results as $row){
+    echo "<tr>";
+      echo "<td>";
+        echo $row['name'];
+      echo "</td>";
+      echo "<td>";
+        echo $row['continent'];
+      echo "</td>";
+      echo "<td>";
+        echo $row['independence_year'];
+      echo "</td>";
+      echo "<td>";
+        echo $row['head_of_state'];
+      echo "</td>";
+    echo "</tr>";
+  }
+  echo "</tbody>
+    </table>";
+}
